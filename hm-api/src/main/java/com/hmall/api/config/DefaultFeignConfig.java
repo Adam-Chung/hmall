cf788295a15b2dae991b7918d66ed39f569e0dd9
Adam Chung <fong850403@gmail.com>
2024-12-29 22:35:42 +0800

    搭配OpenFeign中提供的攔截器實現完整微服務登入解決方案

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,11 +1,29 @@
 package com.hmall.api.config;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+
 import org.springframework.context.annotation.Bean;
+
+import com.hmall.common.utils.UserContext;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL; //完整日誌輸出
     }
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(feign.RequestTemplate requestTemplate) {
+                // 1. 獲取用戶訊息
+                Long userId = UserContext.getUser();
+                requestTemplate.header("user-info", userId == null ? "" : userId.toString());
+                // 2. 將用戶訊息放到request header中
+            }
+        };
+    }
+
 }
